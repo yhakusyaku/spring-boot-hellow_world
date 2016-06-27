24317275dfc58b97c3a846ffbadeec67675375c9
yhakusyaku <yhakusyaku@gmail.com>
2016-06-27 14:53:43 +0900

    spring initializer web

---
+++ src/main/java/yhakusyaku/sample/HellowWorldApplication.java
@@ -0,0 +1,12 @@
+package yhakusyaku.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellowWorldApplication.class, args);
+	}
+}
