21270617a36b316f6297966c5b4272f4d06a86de
yhakusyaku <yhakusyaku@gmail.com>
2016-06-29 11:38:17 +0900

    ログイン画面を追加

---
+++ src/main/java/yhakusyaku/sample/controller/AuthController.java
@@ -0,0 +1,26 @@
+package yhakusyaku.sample.controller;
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthController {
+
+	@RequestMapping("/login")
+	public String login() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		//匿名ユーザーの場合は、ログインページ
+		if (authentication instanceof AnonymousAuthenticationToken) {
+			return "login";
+		}
+		//その他の場合はトップページへリダイレクト
+		//ログインページからアクセスしてきた場合の対応
+		else {
+			return "redirect:/";
+		}
+	}
+
+}
